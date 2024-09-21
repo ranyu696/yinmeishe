06b65782906636699ab9e445f3601296fa547023
ranyu <ranyu696@gmail.com>
2024-09-21 11:51:09 +0800

    修复分类错误

--- src/app/_components/Category.tsx
+++ src/app/_components/Category.tsx
@@ -3,6 +3,36 @@ import Link from 'next/link'
 import { api } from '~/trpc/server'
 import { CategoryType } from '~/types'
 
+const getTypeUrl = (type: CategoryType): string => {
+  switch (type) {
+    case CategoryType.Video:
+      return 'videos'
+    case CategoryType.Novel:
+      return 'novels'
+    case CategoryType.Picture:
+      return 'pictures'
+    case CategoryType.Comic:
+      return 'comics'
+    default:
+      return type
+  }
+}
+
+const getTypeTitle = (type: CategoryType) => {
+  switch (type) {
+    case CategoryType.Video:
+      return '视频'
+    case CategoryType.Novel:
+      return '小说'
+    case CategoryType.Picture:
+      return '图片'
+    case CategoryType.Comic:
+      return '漫画'
+    default:
+      return type
+  }
+}
+
 const CategoryGroup = ({
   type,
   categories,
@@ -10,27 +40,12 @@ const CategoryGroup = ({
   type: CategoryType
   categories: { id: number; name: string }[]
 }) => {
-  const getTypeTitle = (type: CategoryType) => {
-    switch (type) {
-      case CategoryType.Video:
-        return '视频'
-      case CategoryType.Novel:
-        return '小说'
-      case CategoryType.Picture:
-        return '图片'
-      case CategoryType.Comic:
-        return '漫画'
-      default:
-        return type
-    }
-  }
-
   return (
     <div className="mb-2">
       <h2 className="mb-2 text-2xl font-bold">{getTypeTitle(type)}</h2>
       <div className="grid grid-cols-5 gap-2 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-8">
         {categories.map((category) => (
-          <Link key={category.id} href={`/${type}/category/${category.id}`}>
+          <Link key={category.id} href={`/${getTypeUrl(type)}/category/${category.id}`}>
             <Button
               key={category.id}
               size="md"
@@ -57,12 +72,16 @@ const Category = async () => {
   const imageCategories = await api.category.getByType({
     type: CategoryType.Picture,
   })
+  const comicCategories = await api.category.getByType({
+    type: CategoryType.Comic,
+  })
 
   return (
     <div className="mx-auto w-full pt-2">
       <CategoryGroup type={CategoryType.Video} categories={videoCategories} />
       <CategoryGroup type={CategoryType.Novel} categories={novelCategories} />
       <CategoryGroup type={CategoryType.Picture} categories={imageCategories} />
+      <CategoryGroup type={CategoryType.Comic} categories={comicCategories} />
     </div>
   )
 }
